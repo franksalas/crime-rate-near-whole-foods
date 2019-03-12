29e25d8889b8a21668fec578d38a4eb4eabd1393
franksalas <frank.salas@gmail.com>
2019-03-11 22:51:43 -0500

    start of project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='is it safer to live near Whole Foods then other supermarket chain?',
+    author='Francisco Salas',
+    license='MIT',
+)
